a88b384ece9faa4ead38ff1cdd3e8ff6c58cd618
ozonelabrada <shoonixspider@gmail.com>
2021-11-13 01:37:44 +0800

    additional components dashboard, login(design), sidenav, topnav

---
+++ src/components/login/login.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './login.css';
+const Login = () => {
+    return (
+        <div className='log'>
+            <h3>Login Page setup</h3>
+            <form className="loginForm">
+                <div className="loginFormItem">
+                   <label>Email</label>
+                    <input type="text" placeholder="example@gmail.com" />
+                </div>
+                <div className="loginFormItem">
+                   <label>Password</label>
+                    <input type="password" placeholder="*************" />
+                </div>
+                <div className="loginFormItem">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login
