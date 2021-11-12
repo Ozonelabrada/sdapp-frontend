00f61312dabcae07e3620debeb3f8bbdf3c44ddf
ozonelabrada <shoonixspider@gmail.com>
2021-11-13 02:15:01 +0800

    additional components dashboard, login(design), sidenav, topnav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Login from './components/login/login'
+import Home from './components/dashboard/home';
+import SideNav from './components/sideNav/sideNav';
+import TopNav from './components/topnav/topNav';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Login />
+        {/* <TopNav />
+        <SideNav />
+        <Home /> */}
       </header>
     </div>
   );
---
+++ src/components/topnav/topNav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './topNav.css'
+const TopNav = () => {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="top-left">left</div>
+                <div className="top-right">right</div>
+            </div>
+        </div>
+    )
+}
+
+export default TopNav
