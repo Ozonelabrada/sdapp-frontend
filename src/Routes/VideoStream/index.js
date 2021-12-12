3ecbc009a47b42573226166dc062115824a34269
ozonelabrada <shoonixspider@gmail.com>
2021-12-12 19:20:38 +0800

    additional layout stream

--- src/Routes/VideoStream/index.js
+++ src/Routes/VideoStream/index.js
@@ -1,3 +1,4 @@
+import { mdiTab } from '@mdi/js'
 import React from 'react'
 
 const VideoStream = () => {
@@ -18,25 +19,40 @@ const VideoStream = () => {
                         <div className="pt-5 font-bold pl-2 grid-rows-1">Date: 01-26-2022</div>
                         <div className="pl-2 font-bold grid-rows-1">Time:  12:10 PM</div>
                         <div className="w-1/2pb-10s flex flex-col justify-center md:justify-start my-auto h-3/4">
-                            <img className="object-cover rounded-3xl inline-flex justify-self-center md:mt-15  h-5/6  md:block md:m-auto md:w-3/4" alt="" src="./images/socialdistancing.jpg" />
+                            <img className="object-cover  inline-flex justify-self-center md:mt-15  h-5/6  md:block md:m-auto md:w-3/4" alt="" src="./images/socialdistancing.jpg" />
                         </div>
                     </div>
                     <div className="col-auto bg-black w-full h-2/4 text-white">
-                        <div className="grid grid-cols-3 gap-1 text-center h-full border-collapse">
-                            <div className="col-span-3 border-white border border-solid p-2">DATE: 01-26-2022</div>
 
-                            <div className="border-white border border-solid p-2">TIME OF
-                                VIOLATION:</div>
-                            <div className="col-span-2 text-center border-white border border-solid p-2">02</div>
-
-                            <div className="border-white border border-solid p-2">2:10 PM</div>
-                            <div className="col-span-2 border-white border border-solid p-2">02</div>
-
-                            <div className="border-white border border-solid p-2">10:30 AM</div>
-                            <div className="col-span-2 border-white border border-solid p-2">02</div>
-
-                            <div className="border-white border border-solid p-2">9:04 AM</div>
-                            <div className="col-span-2 border-white border border-solid p-2">02</div>
+                        <div className="text-center h-full">
+                            <div className="col-span-3 border-white py-2 inline-flex justify-self-center h-auto">DATE: 01-26-2022</div>
+                            <table className="table-auto border-collapse border border-gray-400 md:w-full col-span-full py-3 mb-2">
+                                <thead className="py-2">
+                                    <tr>
+                                        <th className="border border-gray-300 ...">
+                                            TIME OF
+                                            VIOLATION:
+                                            </th>
+                                        <th className="border border-gray-300 ...">
+                                            NUMBER OF VIOLATORS
+                                            </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td className="border border-gray-300 ...">12:10 PM</td>
+                                        <td className="border border-gray-300 ...">1</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="border border-gray-300 ...">10:30 AM</td>
+                                        <td className="border border-gray-300 ...">3</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="border border-gray-300 ...">9:04 AM</td> 
+                                        <td className="border border-gray-300 ...">1</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
