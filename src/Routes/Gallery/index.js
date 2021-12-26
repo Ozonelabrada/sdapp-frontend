6a6c9a185a140e23dbd34b1d81cd2290e9b6b9b4
ozonelabrada <shoonixspider@gmail.com>
2021-12-26 20:41:43 +0800

    gallery layouts modification

--- src/Routes/Gallery/index.js
+++ src/Routes/Gallery/index.js
@@ -1,41 +1,102 @@
 import React, { Component } from 'react';
-import axios from 'axios'
 
 
 class Gallery extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            comments: []
-        }
-    }
-
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts/1/comments')
-            .then(response => {
-                this.setState({ comments: response.data })
-                console.log(response)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-
-    }
     render() {
-        const { comments } = this.state
         return (
-            <div className="bg-gray-800">
-                {
-                    comments.length ?
-                        comments.map(post =>
-                            <div className="p-5" key={post.id}>
-                                <span className="inset-y-2/4">NAME: {post.name}</span><br />
-                                <span>EMAIL: {post.email}</span><br />
-                                <span className="text-gray-100">BODY: {post.body}</span>
-                            </div>)
-                        : null
-                }
+            <div className="bg-bgAboutL h-screen pt-12 ">
+                <div class="p-0 grid grid-rows-2 ml-10 -mt-14 text-white grid-flow-col gap-4">
+                    <div className='cursor-pointer self-center'>
+                        <div class="grid grid-cols-6 gap-4">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div className='w-80 font-bold text-5xl self-center'>
+                                G A L L E R Y
+                            </div>
+                        </div>
+                    </div>
+                    <div class=" grid grid-cols-6 self-center my-5 md:w-96 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="#f26d6f" viewBox="0 0 24 24" stroke="#b55458">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                        </svg>
+                        <div className=' text-xl text-left float-left  self-center'>YEAR</div>
+                    </div>
+                </div>
+                <div class="grid grid-rows-4 grid-flow-col mx-24 gap-4  text-white">
+                    <div class="grid grid-cols-6 self-center my-5 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="#f26d6f" viewBox="0 0 24 24" stroke="#b55458">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                        </svg>
+                        <div className='pl-0 text-xl float-left self-center'>January</div>
+                    </div>
+                    <div class="grid grid-cols-6 self-center my-5 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="#f26d6f" viewBox="0 0 24 24" stroke="#b55458">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                        </svg>
+                        <div className='pl-0 text-xl float-left self-center'>February</div>
+                    </div>
+                    <div class="grid grid-cols-6 self-center my-5 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="#f26d6f" viewBox="0 0 24 24" stroke="#b55458">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                        </svg>
+                        <div className='pl-0 text-xl float-left self-center'>March</div>
+                    </div>
+                    <div class="grid grid-cols-6 self-center my-5 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="#f26d6f" viewBox="0 0 24 24" stroke="#b55458">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                        </svg>
+                        <div className='pl-0 text-xl float-left self-center'>April</div>
+                    </div>
+                    <div class="grid grid-cols-6 self-center my-5 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="#f26d6f" viewBox="0 0 24 24" stroke="#b55458">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                        </svg>
+                        <div className='pl-0 text-xl float-left self-center'>May</div>
+                    </div>
+                    <div class="grid grid-cols-6 self-center my-5 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="#f26d6f" viewBox="0 0 24 24" stroke="#b55458">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                        </svg>
+                        <div className='pl-0 text-xl float-left self-center'>June</div>
+                    </div>
+                    <div class="grid grid-cols-6 self-center my-5 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="#f26d6f" viewBox="0 0 24 24" stroke="#b55458">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                        </svg>
+                        <div className='pl-0 text-xl float-left self-center'>July</div>
+                    </div>
+                    <div class="grid grid-cols-6 self-center my-5 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="#f26d6f" viewBox="0 0 24 24" stroke="#b55458">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                        </svg>
+                        <div className='pl-0 text-xl float-left self-center'>August</div>
+                    </div>
+                    <div class="grid grid-cols-6 self-center my-5 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="#f26d6f" viewBox="0 0 24 24" stroke="#b55458">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                        </svg>
+                        <div className='pl-0 text-xl float-left self-center'>September</div>
+                    </div>
+                    <div class="grid grid-cols-6 self-center my-5 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="#f26d6f" viewBox="0 0 24 24" stroke="#b55458">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                        </svg>
+                        <div className='pl-0 text-xl float-left self-center'>October</div>
+                    </div>
+                    <div class="grid grid-cols-6 self-center my-5 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="#f26d6f" viewBox="0 0 24 24" stroke="#b55458">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                        </svg>
+                        <div className='pl-0 text-xl float-left self-center'>November</div>
+                    </div>
+                    <div class="grid grid-cols-6 self-center my-5 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-15 w-12" fill="#f26d6f" viewBox="0 0 24 24" stroke="#b55458">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                        </svg>
+                        <div className='pl-0 text-xl float-left self-center'>December</div>
+                    </div>
+                </div>
             </div>
         )
     }
