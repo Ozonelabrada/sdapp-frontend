1e0b17241427e23b193694251089c04597420b9c
ozonelabrada <shoonixspider@gmail.com>
2021-12-30 22:20:49 +0800

    layout enhancement remove overflow

--- src/Routes/VideoStream/index.js
+++ src/Routes/VideoStream/index.js
@@ -1,20 +1,24 @@
 import { mdiTab } from '@mdi/js'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const VideoStream = () => {
     return (
-        <div>
-            <div className="w-full h-screen flex flex-wrap bg-blue-200">
+        <div className='overflow-y-hidden'>
+            <div className="w-full h-screen flex flex-wrap bg-blue-200 pt-20">
                 <div className=" md:w-2/3 flex flex-col">
-                    <div id="" className="relative md:h-5/6 md:w-3/4 md:m-auto p-0">
+                    <div id="" className="relative md:h-4/6 md:w-3/4 md:mr-auto md:ml-auto md:mt-14">
                         <iframe title="aa" className="relative md:w-full pr-0 md:h-full"
                             src="./video/sample.mp4"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
                         />
                     </div>
+                    <div className='text-right pt-10 mr-28 to-black text-lg cursor-pointer md:hover:to-gray-400'><Link to="/gallery">
+                        G A L L E R Y <i class="mdi mdi-arrow-right"></i></Link>
+                    </div>
                 </div>
-                <div className="w-1/3 pb-10 bg-gray-700 md:h-screen">
+                <div className="w-1/3 pb-10 md:h-screen">
                     <div className="col-auto bg-red-50 w-full h-1/2">
                         <div className="pt-5 font-bold pl-2 grid-rows-1">Date: 01-26-2022</div>
                         <div className="pl-2 font-bold grid-rows-1">Time:  12:10 PM</div>
@@ -22,7 +26,7 @@ const VideoStream = () => {
                             <img className="object-cover  inline-flex justify-self-center md:mt-15  h-5/6  md:block md:m-auto md:w-3/4" alt="" src="./images/socialdistancing.jpg" />
                         </div>
                     </div>
-                    <div className="col-auto bg-black w-full h-2/4 text-white">
+                    <div className="col-auto bg-black w-full h-2/5 text-white">
 
                         <div className="text-center h-full">
                             <div className="col-span-3 border-white py-2 inline-flex justify-self-center h-auto">DATE: 01-26-2022</div>
