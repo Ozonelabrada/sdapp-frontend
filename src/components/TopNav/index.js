615b993ca31e63bdefc437983ff114ef9f72649d
ozonelabrada <shoonixspider@gmail.com>
2022-01-05 00:13:03 +0800

    enhancement for responsiveness

--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -16,9 +16,9 @@ const TopNav = () => {
         localStorage.clear()
     }
     return (
-        <div className="lg:container border-b-2 text-gray-600  fixed bg-transparent">
-            <nav className="flex items-center justify-between flex-wrap uppercase">
-                <div className="flex items-center flex-shrink-0  ml-10">{
+        <div className="container text-gray-600 md:fixed md:bg-transparent md:w-screen">
+            <nav className="flex border-b-2 items-center justify-between flex-wrap uppercase w-screen">
+                <div className="flex md:items-center flex-shrink-0  ml-10">{
                             (user && user.token)
                                 ? 
                                 <Link to="/stream" className="flex flex-none items-center font-semibold text-xl tracking-tight"><i className="mdi mdi-eye-circle-outline mdi-24px"></i><span className="m-5">KITA</span></Link>
@@ -29,7 +29,7 @@ const TopNav = () => {
                 <div className="block md:hidden">
                     <button
                         onClick={toggleMenu}
-                        className="flex items-center px-3 py-2 border rounded text-green-600 border-green-400 hover:text-black hover:border-black"
+                        className="flex items-center px-3 py-2 border rounded text-green-600 border-green-400 hover:text-black hover:border-black md:pr-10"
                     >
                         <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
                     </button>
@@ -40,7 +40,7 @@ const TopNav = () => {
                         <Link to="/about" className="block md:inline-block md:mt-4 mx-5"><span className="hover:underline hover:opacity-75">About</span></Link>
                         <Link to="/gallery" className="block md:inline-block md:mt-4 mx-5"><span className="hover:underline hover:opacity-75">Gallery</span></Link>
                     </div>
-                    <div className='pr-10'>
+                    <div className='md:px-10'>
                         {/* <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded   hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a> */}
                         {
                             (user && user.token)
