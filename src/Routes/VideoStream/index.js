90b3d8c1b115fc268f3a08578d96cd2d42e774e8
ozonelabrada <shoonixspider@gmail.com>
2022-01-04 23:27:46 +0800

    make a responsive stream

--- src/Routes/VideoStream/index.js
+++ src/Routes/VideoStream/index.js
@@ -4,21 +4,21 @@ import { Link } from 'react-router-dom'
 
 const VideoStream = () => {
     return (
-        <div className='overflow-y-hidden'>
-            <div className="w-full h-screen flex flex-wrap bg-blue-200 pt-20">
-                <div className=" md:w-2/3 flex flex-col">
-                    <div id="" className="relative md:h-4/6 md:w-3/4 md:mr-auto md:ml-auto md:mt-14">
-                        <iframe title="aa" className="relative md:w-full pr-0 md:h-full"
+        <div className='lg:overflow-y-hidden'>
+            <div className="w-full h-screen md:flex md:flex-wrap bg-blue-200 pt-20">
+                <div className=" md:w-2/3 sm:w-full flex flex-col">
+                    <div id="" className="relative md:h-4/6 sm:h-full md:w-3/4 sm:w-full mr-auto ml-auto md:mt-14">
+                        <iframe title="aa" className="md:relative w-full md:pr-0 h-full"
                             src="./video/sample.mp4"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
                         />
                     </div>
-                    <div className='text-right pt-10 mr-28 to-black text-lg cursor-pointer md:hover:to-gray-400'><Link to="/gallery">
+                    <div className='md:text-right md:pt-10 md:mr-28 to-black md:text-lg cursor-pointer md:hover:to-gray-400'><Link to="/gallery">
                         G A L L E R Y <i class="mdi mdi-arrow-right"></i></Link>
                     </div>
                 </div>
-                <div className="w-1/3 pb-10 md:h-screen">
+                <div className="sm:w-full md:w-1/3 md:pb-10 md:h-screen">
                     <div className="col-auto bg-red-50 w-full h-1/2">
                         <div className="pt-5 font-bold pl-2 grid-rows-1">Date: 01-26-2022</div>
                         <div className="pl-2 font-bold grid-rows-1">Time:  12:10 PM</div>
@@ -26,12 +26,12 @@ const VideoStream = () => {
                             <img className="object-cover  inline-flex justify-self-center md:mt-15  h-5/6  md:block md:m-auto md:w-3/4" alt="" src="./images/socialdistancing.jpg" />
                         </div>
                     </div>
-                    <div className="col-auto bg-black w-full h-2/5 text-white">
+                    <div className="md:col-auto bg-black md:w-full md:h-2/5 text-white">
 
                         <div className="text-center h-full">
-                            <div className="col-span-3 border-white py-2 inline-flex justify-self-center h-auto">DATE: 01-26-2022</div>
-                            <table className="table-auto border-collapse border border-gray-400 md:w-full col-span-full py-3 mb-2">
-                                <thead className="py-2">
+                            <div className="md:col-span-3 border-white md:py-2 md:inline-flex md:justify-self-center md:h-auto">DATE: 01-26-2022</div>
+                            <table className="md:table-auto border-collapse border border-gray-400 w-full md:col-span-full md:py-3 md:mb-2">
+                                <thead className="md:py-2">
                                     <tr>
                                         <th className="border border-gray-300 ...">
                                             TIME OF
