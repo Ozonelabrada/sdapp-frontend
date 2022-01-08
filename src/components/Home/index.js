f81e3839fa0d4a42ebb7f076540b0bb29bac871e
ozonelabrada <shoonixspider@gmail.com>
2022-01-08 17:43:24 +0800

    enhancement home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types'
 
 const Home = props => {
     return (
-        <div className='bg-bgHome pt-10'>
+        <div className='bg-bgHome pt-10 lg:overflow-y-hidden'>
             <div className="w-full h-screen flex flex-wrap">
-                <div className="w-screen md:w-1/2 flex flex-col">
-                <h1 className="p-16 text-6xl font-black leading-snug md:mt-20"
+                <div className="w-screen md:w-1/2 flex flex-colpb-4 2xl:px-24">
+                <h1 className="md:p-16 p-2 xl:text-6xl md:text-5xl text-3xl font-black leading-snug md:mt-20"
                     style={{ fontFamily: "Montserrat" }}>
                     AI-BASED
                     SOCIAL
@@ -16,7 +16,7 @@ const Home = props => {
                     SYSTEM
                 </h1>
                 </div>
-                <div className="w-1/2 pb-10s">
+                <div className="flex-col md:w-1/2 justify-center h-screen justify-self-center block m-auto md:pt-16">
                     <img className="object-cover rounded-3xl inline-flex justify-self-center md:mt-14  h-5/6  md:block md:m-auto md:w-3/4" alt="" src="./images/socialdistancing.jpg" />
                 </div>
             </div>
