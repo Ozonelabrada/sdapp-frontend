b7d063b64812cee72fc7ce258d1644972c12181e
ozonelabrada <shoonixspider@gmail.com>
2022-01-23 20:35:05 +0800

    added for dashboard

--- src/api/endpoints/user.js
+++ src/api/endpoints/user.js
@@ -13,16 +13,9 @@ export const findAllUser = async () => {
         return res.status === 200 ? res.data : null;
     } catch (error) { console.error(error) }
 }
-<<<<<<< HEAD
-
-export const findUser = async (id) => {
-    try {
-        const res = await api.get(`/user/find/${id}`);
-=======
 export const updateUser = async (data) => {
     try {
         const res = await api.patch(`/user/update/${data.id}`,data);
->>>>>>> 4446e3a62fc6237998bb76ce70292bab0f0a5115
         return res.status === 200 ? res.data : null;
     } catch (error) { console.error(error) }
 }
